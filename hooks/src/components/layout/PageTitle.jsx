97fe08e6e401853b5f118adbf588a7d2070fd9c1
vmdev10 <mouraanav@gmail.com>
2021-11-30 16:00:16 -0300

    Create hooks project

---
+++ hooks/src/components/layout/PageTitle.jsx
@@ -0,0 +1,12 @@
+import './PageTitle.css'
+
+import React from 'react'
+
+const PageTitle = props => (
+    <div className={`PageTitle ${props.error ? "error" : ""}`}>
+        <h1>{props.title}</h1>
+        <h2>{props.subtitle}</h2>
+    </div>
+)
+
+export default PageTitle
---
+++ hooks/src/index.js
@@ -0,0 +1,13 @@
+import "./index.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./views/App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
