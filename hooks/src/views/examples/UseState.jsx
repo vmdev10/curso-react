f777b5b442b73211c6ae0f4ddee3325a4bd1445b
vmdev10 <mouraanav@gmail.com>
2021-11-30 16:20:56 -0300

    Using useState Hook

--- hooks/src/views/examples/UseState.jsx
+++ hooks/src/views/examples/UseState.jsx
@@ -1,28 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import PageTitle from '../../components/layout/PageTitle'
-import SectionTitle from '../../components/layout/SectionTitle'
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
 const UseState = (props) => {
-    const [counter, setCounter] = useState(0)
-    return (
-        <div className="UseState">
-            <PageTitle
-                title="Hook UseState"
-                subtitle="Estado em componentes funcionais!"
-            />
-            <SectionTitle title="Exercício #01"/>
-            <div className="center">
-                <span className="text">{counter}</span>
-            <div>
-                <button onClick={() => setCounter(counter - 1)} className="btn">-1</button>
-                <button onClick={() => setCounter(counter + 1)} className="btn">+1</button>
-                {/* A função dentro de setCount retorna o valor atual */}
-                <button onClick={() => setCounter((current) => current + 1000)} className="btn">1000</button>
-            </div>
-            </div>
+  const [counter, setCounter] = useState(0);
+  const [name, setName] = useState('Inicial...')
+  return (
+    <div className="UseState">
+      <PageTitle
+        title="Hook UseState"
+        subtitle="Estado em componentes funcionais!"
+      />
+      <SectionTitle title="Exercício #01" />
+      <div className="center">
+        <span className="text">{counter}</span>
+        <div>
+          <button onClick={() => setCounter(counter - 1)} className="btn">
+            -1
+          </button>
+          <button onClick={() => setCounter(counter + 1)} className="btn">
+            +1
+          </button>
+          {/* A função dentro de setCount retorna o valor */}
+          <button
+            onClick={() => setCounter((current) => current + 1000)}
+            className="btn"
+          >
+            1000
+          </button>
         </div>
-    )
-}
+      </div>
 
-export default UseState
+      <SectionTitle title="Exercício #02"/>
+      <input onChange={(event) => setName(event.target.value)} type="text" className="input" value={name} />
+    </div>
+  );
+};
+
+export default UseState;
