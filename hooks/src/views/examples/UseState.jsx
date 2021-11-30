383a499e725793491e3764cdba172479f83b4ddb
vmdev10 <mouraanav@gmail.com>
2021-11-30 16:09:23 -0300

    Using useState Hook

--- hooks/src/views/examples/UseState.jsx
+++ hooks/src/views/examples/UseState.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseState = (props) => {
+    const [counter, setCounter] = useState(0)
     return (
         <div className="UseState">
             <PageTitle
                 title="Hook UseState"
                 subtitle="Estado em componentes funcionais!"
             />
+            <SectionTitle title="Exercício #01"/>
+            <div className="center">
+                <span className="text">{counter}</span>
+            <div>
+                <button onClick={() => setCounter(counter - 1)} className="btn">-1</button>
+                <button onClick={() => setCounter(counter + 1)} className="btn">+1</button>
+                {/* A função dentro de setCount retorna o valor atual */}
+                <button onClick={() => setCounter((current) => current + 1000)} className="btn">1000</button>
+            </div>
+            </div>
         </div>
     )
 }
